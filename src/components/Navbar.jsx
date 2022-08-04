798ce77a1211a0370f4d593983fe0f49c26cfd28
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-04 20:02:06 +0530

    Navbar with search and icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, styled } from '@mui/material';
-import { FacebookSharp } from '@mui/icons-material';
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  styled,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { FacebookSharp, Mail, Notifications } from '@mui/icons-material';
+
+const StyledToolbar = styled(Toolbar)({
+  display: 'flex',
+  justifyContent: 'space-between',
+});
+
+const Search = styled('div')(({ theme }) => ({
+  backgroundColor: 'white',
+  padding: '0 10px',
+  borderRadius: theme.shape.borderRadius,
+  width: '40%',
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  gap: '20px',
+  alignItems: 'center',
+}));
 
 function Navbar() {
-  const StyledToolbar = styled(Toolbar)({
-    display: 'flex',
-    justifyContent: 'space-between',
-  });
-
   return (
     <AppBar position="sticky">
       <StyledToolbar>
@@ -15,6 +37,22 @@ function Navbar() {
           FB CLONE
         </Typography>
         <FacebookSharp sx={{ display: { xs: 'block', sm: 'none' } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            alt="user avatar"
+            src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg"
+            sx={{ width: 40, height: 40 }}
+          />
+        </Icons>
       </StyledToolbar>
     </AppBar>
   );
