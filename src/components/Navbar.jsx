72df6b70cb55464022183e54111289b4d222fe5a
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-04 17:09:05 +0530

    Navbar Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { AppBar, Toolbar, Typography, styled } from '@mui/material';
+import { FacebookSharp } from '@mui/icons-material';
 
 function Navbar() {
-  return <div>Navbar</div>;
+  const StyledToolbar = styled(Toolbar)({
+    display: 'flex',
+    justifyContent: 'space-between',
+  });
+
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: 'none', sm: 'block' } }}>
+          FB CLONE
+        </Typography>
+        <FacebookSharp sx={{ display: { xs: 'block', sm: 'none' } }} />
+      </StyledToolbar>
+    </AppBar>
+  );
 }
 
 export default Navbar;
