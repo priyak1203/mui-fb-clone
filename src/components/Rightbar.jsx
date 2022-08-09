6ee9b2602ee49fe1199065de48fb4ffa16c12818
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-09 21:26:57 +0530

    imagelist component latest photos

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,15 +1,24 @@
-import { Avatar, AvatarGroup, Typography } from '@mui/material';
+import {
+  Avatar,
+  AvatarGroup,
+  ImageList,
+  ImageListItem,
+  Typography,
+} from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
 
 function Rightbar() {
   return (
     <Box flex={2} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
-      <Box position="fixed">
-        <Typography variant="h6" fontWeight="400">
+      <Box position="fixed" pr={2}>
+        <Typography variant="h6" fontWeight="400" mb={1}>
           Online Friends
         </Typography>
-        <AvatarGroup max={7}>
+        <AvatarGroup
+          max={7}
+          sx={{ justifyContent: 'flex-end', paddingLeft: '10px' }}
+        >
           <Avatar
             alt="Remy Sharp"
             src="https://material-ui.com/static/images/avatar/1.jpg"
@@ -44,6 +53,29 @@ function Rightbar() {
             src="https://material-ui.com//static/images/avatar/9.jpg"
           />
         </AvatarGroup>
+        <Typography variant="h6" fontWeight="400" mt={2}>
+          Latest Photos
+        </Typography>
+        <ImageList cols={3} rowHeight={100} gap={5}>
+          <ImageListItem>
+            <img
+              src="https://material-ui.com/static/images/image-list/breakfast.jpg"
+              alt="breakfast"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://material-ui.com/static/images/image-list/burgers.jpg"
+              alt="burgers"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://material-ui.com/static/images/image-list/camera.jpg"
+              alt="camera"
+            />
+          </ImageListItem>
+        </ImageList>
       </Box>
     </Box>
   );
