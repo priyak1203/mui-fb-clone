2d1fe6a5b37473e12eb8fb6748df8a0c0a8b37d0
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-10 21:57:42 +0530

    add post with icons and textfield

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,12 +1,34 @@
-import { Box, Fab, Modal, styled, Tooltip } from '@mui/material';
-import { Add } from '@mui/icons-material';
+import {
+  Avatar,
+  Box,
+  Fab,
+  Modal,
+  Stack,
+  styled,
+  TextField,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import {
+  Add,
+  Image,
+  EmojiEmotions,
+  PersonAdd,
+  VideoCameraBack,
+} from '@mui/icons-material';
 import React, { useState } from 'react';
-import { flexbox } from '@mui/system';
 
 const StyldModal = styled(Modal)({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
+});
+
+const UserBox = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+  marginBottom: '20px',
 });
 
 const AddPost = () => {
@@ -33,7 +55,32 @@ const AddPost = () => {
         aria-describedby="modal-modal-description"
       >
         <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
-          hello
+          <Typography variant="h6" color="gray" textAlign="center">
+            Create Post
+          </Typography>
+          <UserBox>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://images.pexels.com/photos/3779760/pexels-photo-3779760.jpeg"
+            />
+            <Typography variant="span" fontWeight={500}>
+              Remy Sharp
+            </Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: '100%' }}
+            id="standard-multiline-static"
+            multiline
+            rows={3}
+            placeholder="What's on your mind?"
+            variant="standard"
+          />
+          <Stack direction="row" mt={2} gap={1}>
+            <EmojiEmotions color="primary" />
+            <Image color="secondary" />
+            <VideoCameraBack color="success" />
+            <PersonAdd color="error" />
+          </Stack>
         </Box>
       </StyldModal>
     </>
