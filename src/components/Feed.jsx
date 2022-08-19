08109445db33a9c8b08085935aa4e7af1735b0bf
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-19 19:35:53 +0530

    skeleton component added

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Skeleton, Stack } from '@mui/material';
 import Post from './Post';
 import { userData } from '../utils';
 
 function Feed() {
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, 5000);
   return (
     <Box flex={4} p={2}>
-      {userData.map((data) => {
-        return <Post key={data.id} {...data} />;
-      })}
+      {loading ? (
+        <Stack spacing={1}>
+          <Skeleton variant="text" height={100} />
+          <Skeleton variant="rectangular" height={300} />
+          <Skeleton variant="text" height={150} />
+        </Stack>
+      ) : (
+        <>
+          {userData.map((data) => {
+            return <Post key={data.id} {...data} />;
+          })}
+        </>
+      )}
     </Box>
   );
 }
